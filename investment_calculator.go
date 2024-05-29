1eff324735bc26c4a84b33229976533540b20065
Yann BRONNER <yann.bronner@gmail.com>
2024-05-29 21:38:10 +0200

    investment calculator

---
+++ investment_calculator.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var (
+		investmentAmount   float64 = 1000
+		expectedReturnRate         = 5.5
+		years              float64 = 10
+		futureValue                = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	)
+
+	fmt.Println(futureValue)
+}
